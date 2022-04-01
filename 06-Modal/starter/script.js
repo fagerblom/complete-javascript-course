99b3d33b713e789b7c9954486722e03526c577a2
fagerblom <nora.fagerblom@hyperisland.se>
2022-04-01 09:30:57 +0200

    Working With (querySelector) Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,12 +1,31 @@
 'use strict';
 
-const model = document.querySelector('.modal');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+
+//querySelectorAll > target all elements in class not just first
 const btnCloseModal = document.querySelector('.close-modal');
 
 //querySelectorAll > target all elements in class not just first
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+const openModal = function () {
+  modal.classList.remove('hidden', 'antoher-class');
+  overlay.classList.remove('hidden');
+};
+
+//attach eventhandlers to >>all buttons
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// use one functionn to instead of repeating in each eventListener
+const closeModal = function () {
+  console.log('Button clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
